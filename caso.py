1d0f88e96e23fb9f93c89dce5bb8dfad2cd9dc85
pesto13 <ricola1304@gmail.com>
2023-02-18 23:37:44 +0100

    push

---
+++ caso.py
@@ -0,0 +1,21 @@
+import pyautogui
+from time import sleep
+# Activate Google Chrome window
+# dizionario: key = nome del programma - value la classes
+programs = dict()
+
+for time in range (10):
+    # Get the title of the active tab
+    tab_title = pyautogui.getActiveWindowTitle()
+
+    if(tab_title in programs.keys()):
+        programs[tab_title] += 1
+    else:
+        programs[tab_title] = 1
+
+    print(tab_title)
+    sleep(1)
+
+print(programs)
+
+# Print the title
