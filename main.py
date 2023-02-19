aba9f97e39278c91b86c02eb9e4dabaca5ddaea6
pesto13 <ricola1304@gmail.com>
2023-02-19 02:12:44 +0100

    prende i nomi e vanno

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from time import sleep
 import ctypes
 from ctypes import wintypes
 import psutil
+import json
+import sys
+from dataclasses import dataclass
+
 # Activate Google Chrome window
 # dizionario: key = nome del programma - value la classes
 programs = dict()
 
-browsers = ('chrome.exe', 'msedge.exe')
+browsers = ('chrome', 'firefox', 'msedge', 'iexplore', 'opera', 'brave')
+
+
+@dataclass
+class browserTab():
+    name: str
+    seconds: int
 
 def getBrowserTab():
     return pyautogui.getActiveWindowTitle()
@@ -21,14 +31,23 @@ def getAppName():
     process_name = process.name()
     
     return process_name
-    
 
-for time in range (10):
+def writeOnFile(programs, original_stdout):
+    with open('filename.txt', 'w') as f:
+        sys.stdout = f # Change the standard output to the file we created.
+        print(programs)
+        sys.stdout = original_stdout # Reset the standard output to its original value
+
+count = 0
+while True:
     # Get the title of the active tab
-    
+    count+=1
     app_name = getAppName()
-
-    tab_title = app_name
+    app_name = app_name.split('.')[0]
+    if app_name in browsers:
+        tab_title = getBrowserTab()
+    else:
+        tab_title = app_name
 
     if(tab_title in programs.keys()):
         programs[tab_title] += 1
@@ -38,6 +57,11 @@ for time in range (10):
     print(tab_title)
     sleep(1)
 
-print(programs)
+    if count>=7:
+        original_stdout = sys.stdout
+        writeOnFile(programs, original_stdout)
+        count=0
+    
+
 
 # Print the title
