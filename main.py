8f1a9eeed310179f084aca3d8b0d6de97c0a28da
pesto13 <ricola1304@gmail.com>
2023-02-23 04:14:03 +0100

    aggiunto logica per browsers

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ import sys
 from dataclasses import dataclass
 import ai
 
-import debugpy
-
-import tree as t
+import random
+import string
 
 
 
@@ -22,6 +21,9 @@ class info():
     name: str
     cathegory: str
     seconds: int
+
+    def convert_to_dict(self):
+        print(self.__dict__)
 
     #i nodi nell'albero vengono ordinati per nome dell'applicazione, per poi fare una ricerca più veloce
     """ def __lt__(self, other: object|str):
@@ -63,29 +65,52 @@ def getAppName():
 def writeOnFile(programs, original_stdout):
     with open('filename.txt', 'w') as f:
         sys.stdout = f # Change the standard output to the file we created.
-        print(programs)
+        json_string = ""
+        for p in programs.values():
+            json_string += json.dumps(p.__dict__, indent=4)+"\n"
+        print(json_string)   
         sys.stdout = original_stdout # Reset the standard output to its original value
 
 
 
-def runV1(programs: dict, interval):
+def runV1(programs: dict[info], l: list[str], interval):
     
-    #prendo il nome dell'app, e se è un browser prendo il nome del tab
+    #attualmente non sta venendo usato ne il path ne il tab_title
     app_name, app_path = getAppName()
     if app_name in browsers:
         tab_title = getBrowserTab()
     else:
         tab_title = app_name
 
-
-    #print(app_name)
-
-
-    #se non è presente nel tree allora cerco con ai e aggiungo al tree
     cat:str
+    remember_app: str = ""
+    if app_name in browsers:
+        from collections import Counter
+        # utilizzo di Counter() per contare il numero di occorrenze di ogni elemento
+        counted_list = Counter(l)
+        # utilizzo di most_common() per trovare l'elemento più comune
+        cat = counted_list.most_common(1)[0][0]
+        
+        remember_app = app_name
+        app_name += cat
+
+    #poi procedo ad inserirlo
     if programs.get(app_name) == None:
         print("non trovato")
-        cat = ai.get_application_category(app_name)
+
+        #devo farlo se non è un browser
+        #cat = ai.get_application_category(app_name)
+        
+        # Define the length of the random string
+        length = 5
+
+        # Define the pool of characters to choose from
+        characters = string.ascii_letters + string.digits
+
+        # Generate the random string if is not a browser
+        if remember_app not in browsers:
+            cat = ''.join(random.choice(characters) for _ in range(length))
+
         programs[app_name] = info(app_name, cat, interval)
     
     #altrimenti era già presente
@@ -93,8 +118,16 @@ def runV1(programs: dict, interval):
         print("trovato")
         i = programs.get(app_name)
         i.seconds += interval
-        #print(i)
+        cat = i.cathegory
         programs[app_name] = i
+
+        l.append(cat)
+        
+
+
+    #fineif
+    if len(l)>20:
+        l.pop(0)
 
     
 
@@ -116,27 +149,25 @@ def status(programs: dict):
         print(i)
     
 if __name__ == '__main__':
-    debugpy.listen(5678)
+    
     #piccolomain()
-
-    """ myTree = t.AVLTree()
-    root = None """
-
     interval = 5
-
     programs = dict()
+    l = list()
 
     count = 0
     while True:
         count+=1
 
-        runV1(programs, interval)
-        sleep(interval)
+        runV1(programs, l, interval)
+        
 
         #scrivo su file
-        if count>=7:
+        if count>=1:
             #status(programs)
             original_stdout = sys.stdout
             writeOnFile(programs, original_stdout)
             count=0
+            
+        sleep(interval)
         
