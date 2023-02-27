bd94e96044401696af03cb7fae1dcdcfa264339e
pesto13 <ricola1304@gmail.com>
2023-02-27 07:11:47 +0100

    startup con gcalendar

--- src/gcalendar/code.py
+++ src/gcalendar/code.py
@@ -1,35 +1,63 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-# import datetime
-# import pytz
+from googleapiclient.errors import HttpError
 
-# Carica le credenziali dal file JSON
-creds = Credentials.from_authorized_user_file('path/to/credentials.json')
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-# Costruisci l'oggetto per accedere all'API di Google Calendar
-service = build('calendar', 'v3', credentials=creds)
 
-# Esempio: crea un evento nel calendario
-event = {
-  'summary': 'Test event',
-  'location': 'Rome, Italy',
-  'description': 'A simple test event',
-  'start': {
-    'dateTime': '2023-02-28T09:00:00+01:00',
-    'timeZone': 'Europe/Rome',
-  },
-  'end': {
-    'dateTime': '2023-02-28T10:00:00+01:00',
-    'timeZone': 'Europe/Rome',
-  },
-  'reminders': {
-    'useDefault': True,
-  },
-}
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
-try:
-    event = service.events().insert(calendarId='primary', body=event).execute()
-    print('Event created: %s' % (event.get('htmlLink')))
-except HttpError as error:
-    print('An error occurred: %s' % error)
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+if __name__ == '__main__':
+    main()
